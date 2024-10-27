6e1f4e4f4175f7386cc9c45354bb12b99d1d8341
joshuaApostol2 <joshuaapostol0967@gmail.com>
2024-10-28 01:21:41 +0800

    Update automessage.js

--- events/automessage.js
+++ events/automessage.js
@@ -4,42 +4,45 @@ const cron = require('node-cron');
 let isSending = false;
 
 module.exports = {
-    name: "automessage",
-    description: "Automatically sends a motivational quote every hour.",
+    name: "catfact",
+    description: "edi tanginamo",
     nashPrefix: false,
     version: "1.0.0",
     async onEvent({ api }) {
-        const motivation = async () => {
-            if (isSending) return; 
+        const introduceApp = async () => {
+            if (isSending) return;
             isSending = true;
 
             try {
-                const response = await axios.get("https://nash-rest-api-production.up.railway.app/quote");
-                let quote = response.data.text;
-                const author = response.data.author;
+                const catFactResponse = await axios.get('https://rest-api.joshuaapostol.site/cat-fact');
+                const catFact = catFactResponse.data.data[0];
 
-                quote = quote.replace(/^“|”$/g, '').trim();
+                const catImageResponse = await axios.get('https://rest-api.joshuaapostol.site/cat-image');
+                const catImageUrl = catImageResponse.data.url;
 
-                const formattedQuote = `┌─[ AUTOMESSAGE ]──[ # ]\n` +
-                                       `└───► ${quote}\n\n` +
-                                       `┌─[ AUTHOR ]───[ # ]\n` +
-                                       `└───► ${author}`;
+                const message =`${catFact}\n\n` + `woahhhhh 🫨`;
 
                 const threads = await api.getThreadList(25, null, ['INBOX']);
                 for (const thread of threads) {
                     if (thread.isGroup && thread.name !== thread.threadID) {
-                        await api.sendMessage(formattedQuote, thread.threadID);
+                        const imageStream = await axios({
+                            method: 'get',
+                            url: catImageUrl,
+                            responseType: 'stream',
+                        });
+
+                        await api.sendMessage({ body: message, attachment: imageStream.data }, thread.threadID);
                     }
                 }
             } catch (error) {
-                console.error('Error fetching quote:', error);
+                console.error('Error sending introduction:', error);
             } finally {
-                isSending = false; 
+                isSending = false;
             }
         };
 
         cron.schedule('0 * * * *', () => {
-            motivation();
+            introduceApp();
         });
     },
 };
